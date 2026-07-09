fd1c862fb4fe8a8f5e2bd698d5619564bbeaf906
Shekhar <272598526+poudelsamir@users.noreply.github.com>
2026-07-09 22:31:04 +0545

    dashboard page UI completed

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,11 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { dummyAdminDashboardData, dummyEmployeeDashboardData } from '../assets/assets'
+import Loading from '../components/Loading'
+import EmployeeDashboard from '../components/EmployeeDashboard'
+import AdminDashboard from '../components/AdminDashboard'
 
 const Dashboard = () => {
-  return (
-    <div>
-      dashboard
-    </div>
-  )
+  const[data,setData]=useState(null)
+  const[loading,setLoading]=useState(true)
+
+  useEffect(() => {
+    setData(dummyAdminDashboardData)
+    // setData(dummyEmployeeDashboardData)
+    setTimeout(() => {
+      setLoading(false)
+    }, 1000);   
+  }, [])
+
+  if(loading) return <Loading/>
+  if(!data) return <p className='text-center text-slate-500 py-12'>Failed to load dashboard</p>
+  // console.log(data.role)
+
+  // Admin dummy data have a role key but employee doesnt have so we are doing so!
+  if(data.role === "ADMIN"){
+    return <AdminDashboard data={data} />
+  }else{
+    return <EmployeeDashboard data={data} />
+  }
+  
+ 
 }
 
 export default Dashboard
